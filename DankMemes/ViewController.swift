c8ab87e191373fe36ce7755523ac2c602aa1a4bf
mahahahajan <Tiklum@PulkitApple.local>
2015-11-07 13:32:58 -0800

    Initial Commit

---
+++ DankMemes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DankMemes
+//
+//  Created by Pulkit Mahajan on 11/7/15.
+//  Copyright (c) 2015 Pulkit Mahajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
